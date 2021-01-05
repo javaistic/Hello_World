85233be3a47a0f1c50cd9ab9b609d7c8005b30ec
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2021-01-06 02:43:36 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,14 @@
 //Program to print Hello World
 
 class HelloWorld 
-{
+{ //start of class
+
    public static void main(String[] args) 
-    {
-        System.out.println("Hello, World!"); 
-    }
-}
+   
+    { //start of main() method
+    
+        System.out.println("Hello, World!");//printing Hello World
+        
+    } //end of main() method
+    
+} //end of the class
